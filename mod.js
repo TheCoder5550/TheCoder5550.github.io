17f9284aa0a20712c853f10959254bc18d2620c7
TheCoder5550 <alfons.nilsson@outlook.com>
2016-05-27 15:54:54 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -5,6 +5,28 @@ else {
   setCookie("mod", "d", 365);
 }
 
+if (getCookie("mod") == "m") {
+  window.location = "mobile.html";
+}
+else {
+  window.location = "index.html";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
